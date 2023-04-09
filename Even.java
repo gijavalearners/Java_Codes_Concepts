25dc29f1fafba59156bd54570a58d1dd66702749
gijavalearners <130269716+gijavalearners@users.noreply.github.com>
2023-04-09 10:37:04 +0530

    Add files via upload

---
+++ Even.java
@@ -0,0 +1,16 @@
+class Even
+{
+public static void main(String args[])
+{
+int num1=1;
+int num2=30;
+for(int i=num1;i<=num2;i++)
+{
+if(i%2==0)
+{
+System.out.println("Number "+i+" is Even");
+}
+
+}
+}
+}
