f883fc6fd1f5ec45f5677c228241f7a30b57bcba
gijavalearners <130269716+gijavalearners@users.noreply.github.com>
2023-04-18 12:19:56 +0530

    Add files via upload

---
+++ Third.java
@@ -0,0 +1,24 @@
+    public class Third
+   { 
+    public static void main (String args[ ]) 
+    { 
+        
+         boolean g = true;
+		 char a = 'Z'; 
+         int i = 89; 
+         byte b = 4; 
+         short s = 56; 
+         double d = 4.355453532; 
+         float f = 4.7333434f; 
+		 long h=10000;
+        
+        System.out.println("char: " + a); 
+        System.out.println("integer: " + i); 
+        System.out.println("byte: " + b); 
+        System.out.println("short: " + s); 
+        System.out.println("float: " + f); 
+        System.out.println("double: " + d); 
+		System.out.println("boolean: " + g); 
+		System.out.println("long: " + h); 
+    } 
+}
