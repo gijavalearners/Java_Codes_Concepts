9c067849e4163a7afe0e701b01792d232e53175d
gijavalearners <130269716+gijavalearners@users.noreply.github.com>
2023-04-18 12:20:43 +0530

    Add files via upload

---
+++ Four.java
@@ -0,0 +1,17 @@
+    public class Four
+   { 
+    public static void main (String args[ ]) 
+    { 
+        
+        short a=10;
+		int b=a;				// Implicit Data Type Conversion
+        
+        System.out.println("Short Value: "+a);
+		System.out.println("Integer Value: "+b);
+        
+		long c=2500000;
+		int d=(int)c;			// Explicit Data Type Conversion
+		
+		
+    } 
+}
