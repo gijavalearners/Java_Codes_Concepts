b69bec5e9f85eeda5918f6cf5d789ad334607045
gijavalearners <130269716+gijavalearners@users.noreply.github.com>
2023-04-18 12:19:34 +0530

    Add files via upload

---
+++ Second.java
@@ -0,0 +1,13 @@
+    public class Second
+   { 
+    public static void main (String args[ ]) 
+    { 
+        
+         int a=10;
+        
+        System.out.println("a"); 
+		System.out.println(a);
+		System.out.println("Value of a= "+a+" which is very high");  
+        
+    } 
+}
